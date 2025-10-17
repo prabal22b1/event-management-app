7528541a9f4adf92c7dc09dc925161838c002bfd
prabal <prabal22b1@gmail.com>
2025-10-17 08:45:43 +0000

    Fixed a few issues occured after renaming the api folder

--- server/api/settings.py
+++ server/api/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'server.urls'
+ROOT_URLCONF = 'api.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'server.wsgi.application'
+WSGI_APPLICATION = 'api.wsgi.application'
 
 
 # Database
