bedc1460cd13a73aa511891fa489a0a6784a3df7
Sarthak Nagapurkar <nsarthak99@gmail.com>
2025-10-23 14:27:17 +0000

    Used snackbar, completed event cancellation use case on client side by consuming apis

--- client/src/components/cards/EventDetailCard.jsx
+++ client/src/components/cards/EventDetailCard.jsx
@@ -1,33 +1,103 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardContent, Typography, Button, CardMedia } from '@mui/material';
 import axios from 'axios';
-import { useState } from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import { useParams } from 'react-router-dom';
+import Alert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import { useNavigate } from 'react-router-dom';
 
 function EventDetailCard({ title, description, date, time, location, available_seats, event_type }) {
-    
+    const { id } = useParams();
+    const navigate = useNavigate();
     const [registrationSuccess, setRegistrationSuccess] = useState(false);
+    const [error, setError] = useState(null);
+    const [toast, setToast] = useState(false);
+    const [isRegistered, setRegistrationStatus] = useState(false);
+    const [successMessage, setSuccessMessage] = useState('');
+
+    useEffect(() => {
+    const fetchRegistrationStatus = async () => {
+      try {
+        const token = localStorage.getItem('accessToken'); // Retrieve token if needed
+        const response = await axios.get(`http://localhost:8000/api/v1/events/${id}/check-registration/`, {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-type': 'application/json'
+          }
+        });
+        setRegistrationStatus(response.data.registered);
+      } catch (err) {
+        setError(err);
+      }
+    };
+    fetchRegistrationStatus();
+  }, [id]);
+
+
+    const handleClose = () => {
+        setToast(false);
+        setError(null);
+        navigate('/home')
+    };
+
+    const action = (
+        <React.Fragment>
+            <IconButton
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={handleClose}
+            >
+                <CloseIcon fontSize="small" />
+            </IconButton>
+        </React.Fragment>
+    );
+
     const handleRegister = async () => {
         try {
             const token = localStorage.getItem('accessToken'); // Retrieve token if needed
-            const response = await axios.post(`http://localhost:8000/api/v1/events/${id}/register`, {
+            const response = await axios.post(`http://localhost:8000/api/v1/events/${id}/register/`, null, {
                 headers: {
                     'Authorization': `Bearer ${token}`,
                     'Content-type': 'application/json'
                 }
             })
-            alert('Registration successful!');
             setRegistrationSuccess(true);
+            setRegistrationStatus(true);
+            setSuccessMessage('Registered successfully!Redirecting to home page!')
+            setToast(true);
         } catch (err) {
-            setError(err);
-            console.error("Error fetching event details:", err);
+            setError("You are already registered!");
+            console.error("Error registering for event:", err);
         }
-
     };
+
+    const handleRegistrationCancellation = async () => {
+        try {
+            const token = localStorage.getItem('accessToken'); // Retrieve token if needed
+            const response = await axios.put(`http://localhost:8000/api/v1/events/${id}/register/`, null, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-type': 'application/json'
+                }
+            })
+            setRegistrationStatus(false);
+            setSuccessMessage('Registeration successfully cancelled! Redirecting to home page!')
+            setToast(true);
+        } catch (err) {
+            setError("You are already registered!");
+            console.error("Error registering for event:", err);
+        }
+    };
+
     const eventDateFormatted = new Date(date).toLocaleDateString('en-US', {
         day: 'numeric',
         month: 'short',
         year: 'numeric'
     });
+
     const imageMap = {
         concert: '/concert.jpg',
         webinar: '/webinar.jpg',
@@ -35,12 +105,18 @@ function EventDetailCard({ title, description, date, time, location, available_s
         workshop: '/workshop.jpg'
     };
     const image = imageMap[(event_type || '').toLowerCase()] || 'default.jpg';
+
     return (
         <div className='relative w-fit'>
             <Card style={{ margin: '20px', padding: '20px', width: '500px' }}>
                 {registrationSuccess && (
-                    <Alert severity="success" style={{ backgroundColor: 'green', color: 'white', marginBottom: '20px' }}>
+                    <Alert severity="success" className="mb-4">
                         Registration successful!
+                    </Alert>
+                )}
+                {error && (
+                    <Alert severity="error" className="mb-4">
+                        {error}
                     </Alert>
                 )}
                 <CardContent>
@@ -58,14 +134,12 @@ function EventDetailCard({ title, description, date, time, location, available_s
                             </span>
                         </div>
                     </div>
-                    {/* Title with a larger, bold font */}
                     <Typography variant="h4" gutterBottom style={{ fontWeight: 'bold', fontSize: '2rem', marginBottom: '16px', fontFamily: 'Arial, sans-serif' }}>
                         {title}
                     </Typography>
-                    {/* Description with increased font size */}                     <Typography variant="h6" gutterBottom style={{ marginBottom: '8px', fontFamily: 'Arial, sans-serif' }}>
+                    <Typography variant="h6" gutterBottom style={{ marginBottom: '8px', fontFamily: 'Arial, sans-serif' }}>
                         {description}
                     </Typography>
-                    {/* Details with consistent styling */}
                     <Typography variant="h6" color="textSecondary" gutterBottom style={{ marginBottom: '8px', fontFamily: 'Arial, sans-serif' }}>
                         Date: {eventDateFormatted}
                     </Typography>
@@ -79,7 +153,15 @@ function EventDetailCard({ title, description, date, time, location, available_s
                         Available Seats: {available_seats}
                     </Typography>
                     <div style={{ display: 'flex', justifyContent: 'center' }}>
-                        <Button
+                        {isRegistered ? <Button
+                            variant="contained"
+                            color="primary"
+                            width='30px'
+                            onClick={handleRegistrationCancellation}
+                            style={{ marginTop: '20px' }}
+                        >
+                            Cancel Registration
+                        </Button>: <Button
                             variant="contained"
                             color="primary"
                             width='30px'
@@ -87,11 +169,26 @@ function EventDetailCard({ title, description, date, time, location, available_s
                             style={{ marginTop: '20px' }}
                         >
                             Register
-                        </Button>
+                        </Button>}
+                        <Snackbar
+                            open={toast}
+                            autoHideDuration={3000}
+                            onClose={handleClose}
+                            message={successMessage}
+                            action={action}
+                        />
+                        <Snackbar
+                            open={!!error}
+                            autoHideDuration={3000}
+                            onClose={handleClose}
+                            message={error}
+                            action={action}
+                        />
                     </div>
                 </CardContent>
             </Card>
         </div>
     );
 }
+
 export default EventDetailCard;
