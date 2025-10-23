e044d7bb42612de18b14c2f0b8946dab8f1c3f1d
prabal22b1 <prabal22b1@gmail.com>
2025-10-23 16:14:27 +0000

    Added Spinner, NotFound Page and Unauthorized page

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,17 @@
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+export default function CircularIndeterminate() {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh', // full viewport height
+            }}
+        >
+            <CircularProgress />
+        </Box>
+    );
+}
